8eb1b32188fdc72ce958b659e77a4bbd59b26c62
Daniel Toba <danieloloruntoba681@gmail.com>
2022-09-08 17:00:56 +0100

    Flutterwave integration

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,34 +1,36 @@
 const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
 
 exports.createPayment = async (req, res) => {
-  const { email } = req.body;
   const config = {
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.SECRET_KEY}`,
+      Authorization: `Bearer ${process.env.FLW_SECRET_KEY}`,
     },
   };
+  const body = {
+    tx_ref: uuidv4(),
+    amount: "200",
+    currency: "NGN",
+    redirect_url: "https://www.planetbase.io",
+    customer: {
+      email: "danieloloruntoba681@gmail.com",
+      phonenumber: "09077234932",
+      name: "Daniel Toba",
+    },
+  };
+  const url = "https://api.flutterwave.com/v3/payments";
   try {
-    axios
-      .post(
-        "https://api.paystack.co/transaction/initialize",
-        {
-          email,
-          amount: 200000,
-        },
-        config
-      )
+    const data = axios
+      .post(url, body, config)
       .then((response) => {
-        // console.log(response.data);
-        const data = response.data;
-        res.status(201).json({
-          data,
-        });
+        console.log(response);
       })
       .catch((err) => {
-        res.status(401).json({ err });
+        console.log(err);
       });
-  } catch (error) {
-    res.status(401).json({ error: error.message });
+    res.status(201).json({ data });
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ err });
   }
 };
